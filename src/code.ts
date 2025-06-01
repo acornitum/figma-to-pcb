b72b89b21e9e359265f937e9b105d6b9e4f096bf
aqua <84078890+phthallo@users.noreply.github.com>
2025-05-31 20:44:29 -0400

    colour

--- src/code.ts
+++ src/code.ts
@@ -5,6 +5,18 @@
 
 // This plugin creates rectangles on the screen.
 
+type VectorInfo = {
+  name: string,
+  x: number,
+  y: number
+}
+
+const RED = {
+  "r": 1, 
+  "g": 0, 
+  "b": 0
+}
+
 class GerberFile { 
   constructor () {}
   generateFile(fileName: string, contents: string) {
@@ -13,27 +25,20 @@ class GerberFile {
     }
 }
 
-const f = new GerberFile();
-f.generateFile("name", "contentsskfjsdkl")
-
-// Make sure to close the plugin when you're done. Otherwise the plugin will
-// keep running, which shows the cancel button at the bottom of the screen.
-figma.closePlugin();
-
-
-
-
 // Store vector line locations here
-const vectorLocations: { name: string; x: number; y: number }[] = [];
+const vectorLocations: VectorInfo[] = [];
+const copper: VectorInfo[] = []
 
 // Recursive function to find VECTOR nodes
 function findVectors(node: SceneNode) {
   if (node.type === "VECTOR") {
+    if (node.strokes.filter((n: any) => JSON.stringify(RED) == JSON.stringify(n.color) ).length){
     vectorLocations.push({
       name: node.name,
       x: node.absoluteTransform[0][2],
       y: node.absoluteTransform[1][2],
     });
+    }
   }
 
   if ("children" in node) {
@@ -51,5 +56,13 @@ for (const node of figma.currentPage.children) {
 // Output result to console and close the plugin
 console.log("Vector line locations:", vectorLocations);
 figma.closePlugin(`Found ${vectorLocations.length} vector lines. Check console.`);
+
+
+const f = new GerberFile();
+f.generateFile("name", JSON.stringify(vectorLocations))
+
+// Make sure to close the plugin when you're done. Otherwise the plugin will
+// keep running, which shows the cancel button at the bottom of the screen.
+figma.closePlugin();
 
 
